000eea870bfaee33f82d00020c40d561686126db
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2021-06-11 20:17:19 +0200

    Use XML file loader

--- src/DependencyInjection/BannedExtension.php
+++ src/DependencyInjection/BannedExtension.php
@@ -5,20 +5,19 @@ namespace LongitudeOne\BannedBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class BannedExtension extends Extension
 {
-
     /**
      * @inheritDoc
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader(
+        $loader = new XmlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
-        $loader->load('services.yaml');
+        $loader->load('services.xml');
     }
 }
