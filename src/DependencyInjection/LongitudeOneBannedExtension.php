bbbb74bd0ad54d8e18c1b7357f5367f9a7a15183
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2021-06-12 13:21:59 +0200

    Extension renamed Use YAML file loader

--- src/DependencyInjection/LongitudeOneBannedExtension.php
+++ src/DependencyInjection/LongitudeOneBannedExtension.php
@@ -5,19 +5,19 @@ namespace LongitudeOne\BannedBundle;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class BannedExtension extends Extension
+class LongitudeOneBannedExtension extends Extension
 {
     /**
      * @inheritDoc
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader(
+        $loader = new YamlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 }
